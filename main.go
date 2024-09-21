99f20217c734aa47b743495a8d4f916f71a130fc
jimin <jiminseo0307@gmail.com>
2024-09-22 00:04:50 +0900

    Account + NewAccount golang does not have constructors

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type mascot struct {
-	name string
-	bitrh int
-	details []string
-}
+	"github.com/wozlsla/learngo/accounts"
+)
 
 func main() {
-	details := []string{"eyes", "teeth"}
-	gopher := mascot {name: "go gopher", bitrh: 2009, details: details}
-	fmt.Println(gopher.name)
+	account := accounts.NewAccount("nico")
+	fmt.Println(account)
 }
